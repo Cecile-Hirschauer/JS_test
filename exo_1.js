5cfa7f1ddae58b55abb44d6d726f36e3b715ce1e
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2021-09-18 18:02:18 +0200

    exo1

---
+++ exo_1.js
@@ -0,0 +1,15 @@
+const affiche_n = (list_n) => {
+    for (n of list_n) {
+        if (n % (3 * 5) === 0) {
+            console.log('GarçonFille')
+        } else if (n % 3 == 0) {
+            console.log('Garçon')
+        } else if (n % 5 === 0) {
+            console.log('Fille') 
+        } else {
+            console.log(n)
+        }
+    }
+}
+
+// affiche_n([8, 6, 15, 3, 9, 30])
