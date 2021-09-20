dd3df37b0390e4d3d8636dd641b263fb195ca33a
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2021-09-20 08:04:07 +0200

    fix year if month is january

--- exo_2.js
+++ exo_2.js
@@ -4,8 +4,12 @@ const lastDayOfPreviousMonth = (dates) => {
         let year = date.getFullYear();
         let month = date.getMonth();
         let last_day = new Date(year, month, 0).getDate();
+        if (month === 1){
+            console.log(new Date(year-1, month-1, last_day).toLocaleString().replace(',', '').slice(0, -3));
+
+        }
         console.log(new Date(year, month-1, last_day).toLocaleString().replace(',', '').slice(0, -3));
     }
 }
 
-lastDayOfPreviousMonth(['2004-03-10', '1987-11-04', 'Mar 25 2015'])
+lastDayOfPreviousMonth(['2004-03-10', '1987-11-04', 'Janv 25 2015'])
